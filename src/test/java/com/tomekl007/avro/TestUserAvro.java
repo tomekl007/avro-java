71f2220aebdde99fdf9610e840cf20d89f66702d
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-10-03 20:10:37 +0200

    avro example

---
+++ src/test/java/com/tomekl007/avro/TestUserAvro.java
@@ -0,0 +1,56 @@
+package com.tomekl007.avro;
+
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestUserAvro {
+    @Test
+    public void shouldCreateUserFromAvroSchema() throws IOException {
+        //given
+        User user1 = new User();
+        user1.setName("Alyssa");
+        user1.setFavoriteNumber(256);
+
+        User user2 = new User("Ben", 7, "red");
+
+        User user3 = User.newBuilder()
+                .setName("Charlie")
+                .setFavoriteColor("blue")
+                .setFavoriteNumber(null)
+                .build();
+
+        //when
+        // Serialize user1, user2 and user3 to disk
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
+        DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+        dataFileWriter.create(user1.getSchema(), new File("users.avro"));
+        dataFileWriter.append(user1);
+        dataFileWriter.append(user2);
+        dataFileWriter.append(user3);
+        dataFileWriter.close();
+
+        // Deserialize Users from disk
+        DatumReader<User> userDatumReader = new SpecificDatumReader<User>(User.class);
+        DataFileReader<User> dataFileReader = new DataFileReader<User>(new File("users.avro"), userDatumReader);
+        List<User> result = new LinkedList<User>();
+        while (dataFileReader.hasNext()) {
+            result.add(dataFileReader.next());
+        }
+
+        //then
+        assertThat(result.size()).isEqualTo(3);
+
+    }
+}
